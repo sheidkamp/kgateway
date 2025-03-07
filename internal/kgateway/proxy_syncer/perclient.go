bd4cfd0f5ca241a18844bbaa73687db56fe41fbd
Yuval Kohavi <yuval.kohavi@gmail.com>
2025-03-07 10:24:46 -0500

    convert clusters to resources once

--- internal/kgateway/proxy_syncer/perclient.go
+++ internal/kgateway/proxy_syncer/perclient.go
@@ -2,6 +2,7 @@ package proxy_syncer
 
 import (
 	"fmt"
+	"maps"
 
 	envoycachetypes "github.com/envoyproxy/go-control-plane/pkg/cache/types"
 	envoycache "github.com/envoyproxy/go-control-plane/pkg/cache/v3"
@@ -9,8 +10,42 @@ import (
 	"istio.io/istio/pkg/kube/krt"
 
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/ir"
+	ggv2utils "github.com/kgateway-dev/kgateway/v2/internal/kgateway/utils"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/utils/krtutil"
 )
+
+type clustersWithErrors struct {
+	clusters            envoycache.Resources
+	erroredClusters     []string
+	erroredClustersHash uint64
+	clustersHash        uint64
+	resourceName        string
+}
+
+type endpointsWithUccName struct {
+	endpoints    envoycache.Resources
+	resourceName string
+}
+
+func (c clustersWithErrors) ResourceName() string {
+	return c.resourceName
+}
+
+var _ krt.Equaler[clustersWithErrors] = new(clustersWithErrors)
+
+func (c clustersWithErrors) Equals(k clustersWithErrors) bool {
+	return c.clusters.Version == k.clusters.Version && c.erroredClustersHash == k.erroredClustersHash
+}
+
+func (c endpointsWithUccName) ResourceName() string {
+	return c.resourceName
+}
+
+var _ krt.Equaler[endpointsWithUccName] = new(endpointsWithUccName)
+
+func (c endpointsWithUccName) Equals(k endpointsWithUccName) bool {
+	return c.endpoints.Version == k.endpoints.Version
+}
 
 func snapshotPerClient(
 	l *zap.Logger,
@@ -20,15 +55,66 @@ func snapshotPerClient(
 	endpoints PerClientEnvoyEndpoints,
 	clusters PerClientEnvoyClusters,
 ) krt.Collection[XdsSnapWrapper] {
+
+	clusterSnapshot := krt.NewCollection(uccCol, func(kctx krt.HandlerContext, ucc ir.UniqlyConnectedClient) *clustersWithErrors {
+		clustersForUcc := clusters.FetchClustersForClient(kctx, ucc)
+
+		l.Debug("found perclient clusters", zap.String("client", ucc.ResourceName()), zap.Int("clusters", len(clustersForUcc)))
+
+		if len(clustersForUcc) == 0 {
+			l.Info("no perclient clusters; defer building snapshot", zap.String("client", ucc.ResourceName()))
+			return nil
+		}
+
+		clustersProto := make([]envoycachetypes.ResourceWithTTL, 0, len(clustersForUcc))
+		var clustersHash uint64
+		var erroredClustersHash uint64
+		var erroredClusters []string
+		for _, c := range clustersForUcc {
+			if c.Error == nil {
+				clustersProto = append(clustersProto, envoycachetypes.ResourceWithTTL{Resource: c.Cluster})
+				clustersHash ^= c.ClusterVersion
+			} else {
+				erroredClusters = append(erroredClusters, c.Name)
+				erroredClustersHash ^= ggv2utils.HashString(c.Name)
+			}
+		}
+		clustersVersion := fmt.Sprintf("%d", clustersHash)
+
+		clusterResources := envoycache.NewResourcesWithTTL(clustersVersion, clustersProto)
+
+		return &clustersWithErrors{
+			clusters:        clusterResources,
+			erroredClusters: erroredClusters,
+			clustersHash:    clustersHash,
+			resourceName:    ucc.ResourceName(),
+		}
+	}, krtopts.ToOptions("ClusterResources")...)
+
+	endpointResources := krt.NewCollection(uccCol, func(kctx krt.HandlerContext, ucc ir.UniqlyConnectedClient) *endpointsWithUccName {
+
+		endpointsForUcc := endpoints.FetchEndpointsForClient(kctx, ucc)
+		endpointsProto := make([]envoycachetypes.ResourceWithTTL, 0, len(endpointsForUcc))
+		var endpointsHash uint64
+		for _, ep := range endpointsForUcc {
+			endpointsProto = append(endpointsProto, envoycachetypes.ResourceWithTTL{Resource: ep.Endpoints})
+			endpointsHash ^= ep.EndpointsHash
+		}
+
+		endpointResources := envoycache.NewResourcesWithTTL(fmt.Sprintf("%d", endpointsHash), endpointsProto)
+		return &endpointsWithUccName{
+			endpoints:    endpointResources,
+			resourceName: ucc.ResourceName(),
+		}
+	}, krtopts.ToOptions("EndpointResources")...)
+
 	xdsSnapshotsForUcc := krt.NewCollection(uccCol, func(kctx krt.HandlerContext, ucc ir.UniqlyConnectedClient) *XdsSnapWrapper {
 		maybeMostlySnap := krt.FetchOne(kctx, mostXdsSnapshots, krt.FilterKey(ucc.Role))
 		if maybeMostlySnap == nil {
 			l.Debug("snapshotPerClient - snapshot missing", zap.String("proxyKey", ucc.Role))
 			return nil
 		}
-		clustersForUcc := clusters.FetchClustersForClient(kctx, ucc)
-
-		l.Debug("found perclient clusters", zap.String("client", ucc.ResourceName()), zap.Int("clusters", len(clustersForUcc)))
+		clustersForUcc := krt.FetchOne(kctx, clusterSnapshot, krt.FilterKey(ucc.ResourceName()))
 
 		// HACK
 		// https://github.com/solo-io/gloo/pull/10611/files#diff-060acb7cdd3a287a3aef1dd864aae3e0193da17b6230c382b649ce9dc0eca80b
@@ -42,43 +128,29 @@ func snapshotPerClient(
 		// with that computation and will almost always lose.
 		// While we're looking for a way to make this ordering predictable
 		// to avoid hacks like this, it will do for now.
-		if len(clustersForUcc) == 0 {
+		if clustersForUcc == nil {
 			l.Info("no perclient clusters; defer building snapshot", zap.String("client", ucc.ResourceName()))
 			return nil
 		}
-
-		clustersProto := make([]envoycachetypes.ResourceWithTTL, 0, len(clustersForUcc)+len(maybeMostlySnap.Clusters))
-		var clustersHash uint64
-		var erroredClusters []string
-		for _, c := range clustersForUcc {
-			if c.Error == nil {
-				clustersProto = append(clustersProto, envoycachetypes.ResourceWithTTL{Resource: c.Cluster})
-				clustersHash ^= c.ClusterVersion
-			} else {
-				erroredClusters = append(erroredClusters, c.Name)
-			}
-		}
-		clustersProto = append(clustersProto, maybeMostlySnap.Clusters...)
-		clustersHash ^= maybeMostlySnap.ClustersHash
-		clustersVersion := fmt.Sprintf("%d", clustersHash)
-
-		endpointsForUcc := endpoints.FetchEndpointsForClient(kctx, ucc)
-		endpointsProto := make([]envoycachetypes.ResourceWithTTL, 0, len(endpointsForUcc))
-		var endpointsHash uint64
-		for _, ep := range endpointsForUcc {
-			endpointsProto = append(endpointsProto, envoycachetypes.ResourceWithTTL{Resource: ep.Endpoints})
-			endpointsHash ^= ep.EndpointsHash
-		}
+		clusterResources := clustersForUcc.clusters
+		endpointResources := krt.FetchOne(kctx, endpointResources, krt.FilterKey(ucc.ResourceName()))
 
 		snap := XdsSnapWrapper{}
+		if len(maybeMostlySnap.Clusters) > 0 {
+			clustersProto := make(map[string]envoycachetypes.ResourceWithTTL, len(maybeMostlySnap.Clusters)+len(clustersForUcc.clusters.Items))
+			maps.Copy(clustersProto, clustersForUcc.clusters.Items)
+			for _, item := range maybeMostlySnap.Clusters {
+				clustersProto[envoycache.GetResourceName(item.Resource)] = item
+			}
+			clusterResources.Version = fmt.Sprintf("%d", clustersForUcc.clustersHash^maybeMostlySnap.ClustersHash)
+			clusterResources.Items = clustersProto
+		}
 
-		clusterResources := envoycache.NewResourcesWithTTL(clustersVersion, clustersProto)
-		endpointResources := envoycache.NewResourcesWithTTL(fmt.Sprintf("%d", endpointsHash), endpointsProto)
-		snap.erroredClusters = erroredClusters
+		snap.erroredClusters = clustersForUcc.erroredClusters
 		snap.proxyKey = ucc.ResourceName()
 		snapshot := &envoycache.Snapshot{}
 		snapshot.Resources[envoycachetypes.Cluster] = clusterResources //envoycache.NewResources(version, resource)
-		snapshot.Resources[envoycachetypes.Endpoint] = endpointResources
+		snapshot.Resources[envoycachetypes.Endpoint] = endpointResources.endpoints
 		snapshot.Resources[envoycachetypes.Route] = maybeMostlySnap.Routes
 		snapshot.Resources[envoycachetypes.Listener] = maybeMostlySnap.Listeners
 		//envoycache.NewResources(version, resource)
@@ -87,7 +159,7 @@ func snapshotPerClient(
 			zap.Stringer("Listeners", resourcesStringer(maybeMostlySnap.Listeners)),
 			zap.Stringer("Clusters", resourcesStringer(clusterResources)),
 			zap.Stringer("Routes", resourcesStringer(maybeMostlySnap.Routes)),
-			zap.Stringer("Endpoints", resourcesStringer(endpointResources)),
+			zap.Stringer("Endpoints", resourcesStringer(endpointResources.endpoints)),
 		)
 
 		return &snap
