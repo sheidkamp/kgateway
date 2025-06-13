f6ca071fb17ee31ee58d3029a18e30aa04b6a63d
David Haifley <david.haifley@solo.io>
2025-06-13 01:39:59 -0400

    feat(metrics): add unit tests for krt collection metrics
    Signed-off-by: David Haifley <david.haifley@solo.io>

--- internal/kgateway/proxy_syncer/metrics_test.go
+++ internal/kgateway/proxy_syncer/metrics_test.go
@@ -1,12 +1,23 @@
-package proxy_syncer_test
+package proxy_syncer
 
 import (
 	"testing"
 	"time"
 
+	clusterv3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
+	corev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	endpointv3 "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"
+	envoycachetypes "github.com/envoyproxy/go-control-plane/pkg/cache/types"
+	envoycache "github.com/envoyproxy/go-control-plane/pkg/cache/v3"
 	"github.com/stretchr/testify/assert"
+	"istio.io/istio/pkg/kube/krt"
+	"istio.io/istio/pkg/kube/krt/krttest"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
 
-	. "github.com/kgateway-dev/kgateway/v2/internal/kgateway/proxy_syncer"
+	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/ir"
+	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/krtcollections"
+	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/utils/krtutil"
 	"github.com/kgateway-dev/kgateway/v2/pkg/metrics"
 	"github.com/kgateway-dev/kgateway/v2/pkg/metrics/metricstest"
 )
@@ -146,4 +157,177 @@ func TestStatusSyncMetricsNotActive(t *testing.T) {
 
 	currentMetrics.AssertMetricNotExists("kgateway_status_syncer_status_syncs_total")
 	currentMetrics.AssertMetricNotExists("kgateway_status_syncer_status_sync_duration_seconds")
+}
+
+func TestXDSSnapshotsCollectionMetrics(t *testing.T) {
+	testCases := []struct {
+		name   string
+		inputs []any
+	}{
+		{
+			name: "NewProxySyncer",
+			inputs: []any{
+				ir.NewUniqlyConnectedClient(
+					"kgateway-kube-gateway-api~ns~test",
+					"ns",
+					map[string]string{"a": "b"},
+					ir.PodLocality{
+						Zone:    "zone1",
+						Region:  "region1",
+						Subzone: "subzone1",
+					}),
+				GatewayXdsResources{
+					NamespacedName: types.NamespacedName{
+						Name:      "test",
+						Namespace: "ns",
+					},
+					Routes: envoycache.Resources{
+						Version: "v1",
+						Items: map[string]envoycachetypes.ResourceWithTTL{
+							"test-route": {TTL: ptr.To(time.Minute)},
+						},
+					},
+					Listeners: envoycache.Resources{
+						Version: "v1",
+						Items: map[string]envoycachetypes.ResourceWithTTL{
+							"test-listener": {TTL: ptr.To(time.Minute)},
+						},
+					},
+					Clusters: []envoycachetypes.ResourceWithTTL{{
+						Resource: &clusterv3.Cluster{
+							Name: "test",
+							TransportSocketMatches: []*clusterv3.Cluster_TransportSocketMatch{{
+								Name: "test",
+							}},
+						},
+						TTL: ptr.To(time.Minute),
+					}},
+				},
+				UccWithEndpoints{
+					Client: ir.NewUniqlyConnectedClient(
+						"kgateway-kube-gateway-api~ns~test",
+						"ns",
+						map[string]string{"a": "b"},
+						ir.PodLocality{
+							Zone:    "zone1",
+							Region:  "region1",
+							Subzone: "subzone1",
+						}),
+					Endpoints: &endpointv3.ClusterLoadAssignment{
+						ClusterName: "test",
+						Endpoints: []*endpointv3.LocalityLbEndpoints{
+							{
+								Locality: &corev3.Locality{
+									Region:  "region1",
+									Zone:    "zone1",
+									SubZone: "subzone1",
+								},
+								LbEndpoints: []*endpointv3.LbEndpoint{{
+									HostIdentifier: &endpointv3.LbEndpoint_Endpoint{
+										Endpoint: &endpointv3.Endpoint{
+											Address: &corev3.Address{
+												Address: &corev3.Address_SocketAddress{
+													SocketAddress: &corev3.SocketAddress{
+														Address: "",
+														PortSpecifier: &corev3.SocketAddress_PortValue{
+															PortValue: 8080,
+														},
+													},
+												},
+											},
+										},
+									},
+								}},
+							},
+						},
+					},
+				},
+				uccWithCluster{
+					Client: ir.NewUniqlyConnectedClient(
+						"kgateway-kube-gateway-api~ns~test",
+						"ns",
+						map[string]string{"a": "b"},
+						ir.PodLocality{
+							Zone:    "zone1",
+							Region:  "region1",
+							Subzone: "subzone1",
+						}),
+					Cluster: &clusterv3.Cluster{
+						TransportSocketMatches: []*clusterv3.Cluster_TransportSocketMatch{{
+							Name: "test",
+						}},
+						Name: "test",
+					},
+					Name: "test",
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ResetMetrics()
+
+			mock := krttest.NewMock(t, tc.inputs)
+			mockUcc := krttest.GetMockCollection[ir.UniqlyConnectedClient](mock)
+			mockGatewayXDSResorces := krttest.GetMockCollection[GatewayXdsResources](mock)
+			mockUccWithEndpoints := krttest.GetMockCollection[UccWithEndpoints](mock)
+			mockUccWithCluster := krttest.GetMockCollection[uccWithCluster](mock)
+
+			c := snapshotPerClient(krtutil.KrtOptions{}, mockUcc, mockGatewayXDSResorces,
+				PerClientEnvoyEndpoints{
+					endpoints: mockUccWithEndpoints,
+					index: krt.NewIndex(mockUccWithEndpoints, func(ucc UccWithEndpoints) []string {
+						return []string{ucc.Client.ResourceName()}
+					}),
+				},
+				PerClientEnvoyClusters{
+					clusters: mockUccWithCluster,
+					index: krt.NewIndex(mockUccWithCluster, func(ucc uccWithCluster) []string {
+						return []string{ucc.Client.ResourceName()}
+					}),
+				},
+				krtcollections.NewCollectionMetricsRecorder("ClientXDSSnapshots"))
+
+			c.WaitUntilSynced(nil)
+			time.Sleep(5 * time.Millisecond) // Allow some time for events to process.
+
+			gathered := metricstest.MustGatherMetrics(t)
+
+			gathered.AssertMetricsLabels("kgateway_collection_transforms_total", [][]metrics.Label{{
+				{Name: "collection", Value: "ClientXDSSnapshots"},
+				{Name: "result", Value: "success"},
+			}})
+
+			gathered.AssertMetricCounterValues("kgateway_collection_transforms_total", []float64{1})
+
+			gathered.AssertMetricsLabels("kgateway_collection_transform_duration_seconds", [][]metrics.Label{{
+				{Name: "collection", Value: "ClientXDSSnapshots"},
+			}})
+
+			gathered.AssertMetricGaugeValues("kgateway_collection_resources", []float64{1, 1, 1, 1})
+
+			gathered.AssertMetricsLabels("kgateway_collection_resources", [][]metrics.Label{{
+				{Name: "collection", Value: "ClientXDSSnapshots"},
+				{Name: "name", Value: "test"},
+				{Name: "namespace", Value: "ns"},
+				{Name: "resource", Value: "Cluster"},
+			}, {
+				{Name: "collection", Value: "ClientXDSSnapshots"},
+				{Name: "name", Value: "test"},
+				{Name: "namespace", Value: "ns"},
+				{Name: "resource", Value: "Endpoint"},
+			}, {
+				{Name: "collection", Value: "ClientXDSSnapshots"},
+				{Name: "name", Value: "test"},
+				{Name: "namespace", Value: "ns"},
+				{Name: "resource", Value: "Listener"},
+			}, {
+				{Name: "collection", Value: "ClientXDSSnapshots"},
+				{Name: "name", Value: "test"},
+				{Name: "namespace", Value: "ns"},
+				{Name: "resource", Value: "Route"},
+			}})
+		})
+	}
 }
