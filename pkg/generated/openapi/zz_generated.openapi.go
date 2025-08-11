5657ee69e02f7c17f5a03c03994d1d57f4227755
sheidkamp <seth.heidkamp@gmail.com>
2025-08-11 10:54:30 -0400

    generate
    Signed-off-by: sheidkamp <seth.heidkamp@gmail.com>

--- pkg/generated/openapi/zz_generated.openapi.go
+++ pkg/generated/openapi/zz_generated.openapi.go
@@ -3657,7 +3657,8 @@ func schema_kgateway_v2_api_v1alpha1_HTTPListenerPolicy(ref common.ReferenceCall
 	return common.OpenAPIDefinition{
 		Schema: spec.Schema{
 			SchemaProps: spec.SchemaProps{
-				Type: []string{"object"},
+				Description: "HTTPListenerPolicy is intended to be used for configuring the Envoy `HttpConnectionManager` and any other config or policy that should map 1-to-1 with a given HTTP listener, such as the Envoy health check HTTP filter. Currently these policies can only be applied per `Gateway` but support for `Listener` attachment may be added in the future. See https://github.com/kgateway-dev/kgateway/issues/11786 for more details.",
+				Type:        []string{"object"},
 				Properties: map[string]spec.Schema{
 					"kind": {
 						SchemaProps: spec.SchemaProps{
@@ -3751,7 +3752,7 @@ func schema_kgateway_v2_api_v1alpha1_HTTPListenerPolicySpec(ref common.Reference
 	return common.OpenAPIDefinition{
 		Schema: spec.Schema{
 			SchemaProps: spec.SchemaProps{
-				Description: "HTTPListenerPolicySpec defines the desired state of a HTTP listener policy. It is intended to be primarily used for Http Connection Manager configuration, though it has been extended to support other filters that should map 1-to-1 with HCM configuration, such as the health check HTTP filter. Current these policies can only be applied per gateway, and it is expected that support for per-listener attachment will be added in the future. See https://github.com/kgateway-dev/kgateway/issues/11786 for more details.",
+				Description: "HTTPListenerPolicySpec defines the desired state of a HTTP listener policy.",
 				Type:        []string{"object"},
 				Properties: map[string]spec.Schema{
 					"targetRefs": {
