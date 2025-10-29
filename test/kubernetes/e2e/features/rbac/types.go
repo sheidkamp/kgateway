c6ceceb44f49e31fff727f646d99df007a80fc9d
sheidkamp <seth.heidkamp@gmail.com>
2025-10-29 14:36:12 -0400

    RBAC test
    Signed-off-by: sheidkamp <seth.heidkamp@gmail.com>

--- test/kubernetes/e2e/features/rbac/types.go
+++ test/kubernetes/e2e/features/rbac/types.go
@@ -18,8 +18,9 @@ import (
 
 var (
 	// manifests
-	setupManifest = filepath.Join(fsutils.MustGetThisDir(), "testdata", "setup.yaml")
-	rbacManifest  = filepath.Join(fsutils.MustGetThisDir(), "testdata", "cel-rbac.yaml")
+	setupManifest            = filepath.Join(fsutils.MustGetThisDir(), "testdata", "setup.yaml")
+	rbacManifest             = filepath.Join(fsutils.MustGetThisDir(), "testdata", "cel-rbac.yaml")
+	rbacManifestWithSections = filepath.Join(fsutils.MustGetThisDir(), "testdata", "cel-rbac-section.yaml")
 	// Core infrastructure objects that we need to track
 	gatewayObjectMeta = metav1.ObjectMeta{
 		Name:      "gw",
@@ -36,13 +37,21 @@ var (
 		Body:       gomega.ContainSubstring("RBAC: access denied"),
 	}
 
+	commonSetupManifests = []string{defaults.HttpbinManifest, defaults.CurlPodManifest}
 	// Base test setup - common infrastructure for all tests
 	setup = base.TestCase{
-		Manifests: []string{setupManifest, defaults.HttpbinManifest, defaults.CurlPodManifest},
+		Manifests: append([]string{setupManifest}, commonSetupManifests...),
 	}
 
 	// Individual test cases - test-specific manifests and resources
 	testCases = map[string]*base.TestCase{
+		"TestRBACHeaderAuthorizationWithRouteLevelRBAC": {
+			Manifests: []string{rbacManifestWithSections},
+			MinGatewayApiVersion: map[base.GatewayApiChannel]*base.GwApiVersion{
+				base.GwApiChannelExperimental: &base.GwApiV1_2_0, // HTTPRoutes.spec.rules[].name was added in 1.2 experimental
+				base.GwApiChannelStandard:     &base.GwApiV1_3_0, // HTTPRoutes.spec.rules[].name was added to standard in 1.3
+			},
+		},
 		"TestRBACHeaderAuthorization": {
 			Manifests: []string{rbacManifest},
 		},
