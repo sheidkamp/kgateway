1a9f5a12774ba09b58939254e7993e7cbb40ff96
Omar Hammami <58956785+puertomontt@users.noreply.github.com>
2026-05-28 13:32:30 -0500

    add test for https listener with retries (#14127)
    Signed-off-by: omar <omar.hammami@solo.io>

--- pkg/kgateway/translator/gateway/gateway_translator_test.go
+++ pkg/kgateway/translator/gateway/gateway_translator_test.go
@@ -1435,6 +1435,17 @@ func TestBasic(t *testing.T) {
 		})
 	})
 
+	t.Run("TrafficPolicy HTTPS retry", func(t *testing.T) {
+		test(t, translatorTestCase{
+			inputFiles: []string{"traffic-policy/https-retry.yaml"},
+			outputFile: "traffic-policy/https-retry.yaml",
+			gwNN: types.NamespacedName{
+				Namespace: "default",
+				Name:      "example-gateway",
+			},
+		})
+	})
+
 	t.Run("TrafficPolicy timeout attached to GRPCRoute", func(t *testing.T) {
 		test(t, translatorTestCase{
 			inputFiles: []string{"traffic-policy/grpcroute-timeout.yaml"},
