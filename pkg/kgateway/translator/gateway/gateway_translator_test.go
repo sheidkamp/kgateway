599c83ddfa0dd09d08feac771bc3d5736e15d414
Omar Hammami <58956785+puertomontt@users.noreply.github.com>
2026-07-09 15:31:05 -0500

    [v2.3.x] apply TrafficPolicy retries per route (#14304)
    Signed-off-by: omar <omar.hammami@solo.io>

--- pkg/kgateway/translator/gateway/gateway_translator_test.go
+++ pkg/kgateway/translator/gateway/gateway_translator_test.go
@@ -1339,6 +1339,17 @@ func TestBasic(t *testing.T) {
 		})
 	})
 
+	t.Run("TrafficPolicy HTTPS retry", func(t *testing.T) {
+		test(t, translatorTestCase{
+			inputFile:  "traffic-policy/https-retry.yaml",
+			outputFile: "traffic-policy/https-retry.yaml",
+			gwNN: types.NamespacedName{
+				Namespace: "default",
+				Name:      "example-gateway",
+			},
+		})
+	})
+
 	t.Run("TrafficPolicy timeout attached to GRPCRoute", func(t *testing.T) {
 		test(t, translatorTestCase{
 			inputFile:  "traffic-policy/grpcroute-timeout.yaml",
